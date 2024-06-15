6b7e14ecf8e221eebd016046b3d00dc96f6ed889
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-06-15 18:45:26 +0200

    Update new-multi-plaza.lua

--- new-multi-plaza.lua
+++ new-multi-plaza.lua
@@ -1,33 +1,13 @@
-getgenv().autoPlaza = {
-    Sniper = {
-        Enabled = true,
-
-        DISCORD_ID = "667064890359545917",
-        WEBHOOK_URL = "https://discord.com/api/webhooks/1201931621570445322/yKIPu11QYkWCH9IolW4m159LOV0cF8m7z-IY4XDWRDez-Q3qViCLPqv6iPH7XrBuUpsN",
-        SHOW_USER = false,
-
-        SEND_SNPES = true,
-        PING_SNPES = false,
-
-        SEND_FAILS = true,
-        PING_FAILS = true,
-
-        PING_ALL_EXCLUSIVES = true,
-        BUY_ANY_EXCLUSIVE_PRICE = 2000000,
-        BUY_ANY_HUGE_PRICE = 60000000,
-        BUY_ANY_TITANIC_PRICE = 5000000000,
-
-        BUY_CUSTOM = {
-            {Class = "Pet", Item = {id = "Blazing Shark"}, Cost = 2500000},
-            {Class = "Pet", Item = {id = "Blazing Corgi"}, Cost = 3300000},
-        },
-
-        MIN_CANDIDATES = 3, -- (for stats tracker)
-        STATS_TRACKER = true,
-
-        MIN_BOOTH_CHECKS = 1,
-        MIN_FOUND_SERVERS = 3,
-        SERVER_HOPPER = true,
+getgenv()._S = {
+    Rules = {
+        {Class = "Pet", Item = { id = "Blazing Shark", }, Price = 2500000},
+        {Class = "Pet", Item = { id = "Blazing Corgi", }, Price = 3300000},
+    },
+    Webhooks = {
+        Send = true,
+        Webhook = "https://discord.com/api/webhooks/1249413828547182742/VUCfE51nqu43KBRgI1qqyhoZd7AeGZtrQgsr1AADWAABe1xSFk62t77zHTCJtGqFCzk3",
+        ShowUsername = false,
     }
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0f103ef58ec991b5be6c9a2dcf83dad1.lua"))()
+script_key="zLjgucpjtSDEcqpnMZGRwoKpFdoBYvJI";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/62e6fec03d0dfa254ff6eab3e83b4b08.lua"))()
